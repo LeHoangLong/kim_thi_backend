272abc726ef7232a246914d71c8283a458ee772e
Long <le.hoang.long@outlook.com>
2021-10-23 00:59:31 +0800

    Ignore build folder and add dockerfile

--- build/src/middleware/Context.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.generateContext = void 0;
-//must be at the front of middleware chain
-function generateContext(request, response, next) {
-    request.context = {};
-    next();
-}
-exports.generateContext = generateContext;
--- build/src/routes/PageRoute.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const PageView_1 = require("../view/PageView");
-const router = express_1.Router();
-router.get('/search', PageView_1.productSearchPage);
-router.get('/products/:productId', PageView_1.productDetailPage);
-router.get('/', PageView_1.productSummaryPage);
-exports.default = router;
