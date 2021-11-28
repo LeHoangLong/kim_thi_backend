5c0d921557766f6b1388be70f1f3f7bec90abddb
Le Hoang Long <le.hoang.long@outlook.com>
2021-11-28 16:05:12 +0800

    Enabling cors

--- src/index.ts
+++ src/index.ts
@@ -30,6 +30,12 @@ const jwtAuthentication = myContainer.get<JwtAuthenticator>(TYPES.JWT_AUTHENTICA
 
 app.set('view engine', 'ejs');
 app.use(cors())
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(express.json());
 app.use(cookies());
 app.use(generateContext);
